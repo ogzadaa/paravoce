a1ecb94e475b40d8e909c18a17c0e0372b67dc13
Agent06xl <pedrolopesdejesus309@gmail.com>
2025-01-08 20:44:13 -0300

    Add static file serving for script.js and styles.css

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname)));
+
 // Conexão com o MongoDB
 mongoose
   .connect(
